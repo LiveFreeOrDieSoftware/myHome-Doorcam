cc3aaa0c52edfa09c47a4da4806932cdd9d923bf
mlambi <13841882+mlambi@users.noreply.github.com>
2020-06-23 16:03:35 -0400

    Update update_bashrc.sh

--- update_bashrc.sh
+++ update_bashrc.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+echo "This script will add lines to the .bashrc file to support Python virtual environments and the virtual environment wrapper."
+echo "After amending the .bashrc file the new settings will be invoked with the 'source' command."
+echo "Subsequent logins will automatically invoke this and will not need to be 'source'd."
+
+echo "# These lines added to support Python virtual environments by myHome-Doorcam" >> $HOME/.bashrc
 echo "export WORKON_HOME=$HOME/.virtualenvs" >> $HOME/.bashrc
 echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> $HOME/.bashrc
-echo "source /usr/local/bin/virtualenvwrapper.sh" >> $HOME/.bashrc
+echo "export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv" >> $HOME/.bashrc
 echo "export VIRTUALENVWRAPPER_ENV_BIN_DIR=bin" >> $HOME/.bashrc
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> $HOME/.bashrc
+
+source $HOME/.bashrc
